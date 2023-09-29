0b9c81e9fec8ec16ad97e2243ed2d40a0374e8d4
Moses-Oyelade <oyelademoses@gmail.com>
2023-09-29 10:54:27 +0100

    All passed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        
+        print(f"{i}")
+        i -= 1
+        
+    print(f"Happy New Year!")
+    
+    
+print(happy_new_year())
 
-def square_integers(int_list):
+list = [1, 2, 3, 4, 5]
+
+def square_integers(list):
     # code goes here!
-    pass
+    new_list = []
+    for num in list:
+        new_list.append(num**2)
+    return new_list
+
+
+# print(square_integers(list))
+
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 100
+    for i in range(1, 101):
+        if i%3==0 and i%5==0 :
+            print(f"FizzBuzz")
+        elif i%3==0:
+            print(f"Fizz")
+        elif i%5==0:
+            print(f"Buzz")
+        else:
+            print(i)
+    return i
+    
+# print(fizzbuzz())
+        
+
+
